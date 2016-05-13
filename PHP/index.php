a5d3ffbb69baaaa16d7963f6b502328e729f630e
Vaidakovich <wox.chentsu@gmail.com>
2016-05-13 18:24:18 +0300

    ссылки из корневой директории заголовок дз2

--- PHP/index.php
+++ PHP/index.php
@@ -5,25 +5,44 @@
  * Date: 05.05.2016
  * Time: 22:38
  */
-
 define( 'BR', "<br>");
-$x="Hello there";
-//BR;
-$X='Hello here';
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP home works</title>
+</head>
+<body>
+    <div >
+        <ul>
+            <li> PHP 1
+                <ul>
+                    <li><a href="PHP1/DZ1_Vaidakovich/index.php">HW1</a></li>
+                    <li><a href="PHP1/DZ2_Vaidakovich/index.php">HW2</a></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </li>
+            <li> PHP 2
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
 
 
-echo $X;
-echo '<br>';
-echo $x;
-echo BR.BR;
-
-$a = "2 Тугрика ";
-$b = '3 Тугрика ';
-$c = 'пять Тугриков ';
-
-
-echo $a.$b.$c;
-echo '<br>', BR;
-echo "a+b = ", $a+$b;
-echo BR;
-echo "a+b+c =", $a+$b+$c;
