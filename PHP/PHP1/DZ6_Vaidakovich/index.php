e61fa1a401c9f6dd18be2bb539106405cf2a8d3f
Vaidakovich <wox.chentsu@gmail.com>
2016-06-02 17:59:14 +0300

    дз6  отладить русские символы в  названии файлов

--- PHP/PHP1/DZ6_Vaidakovich/index.php
+++ PHP/PHP1/DZ6_Vaidakovich/index.php
@@ -20,61 +20,113 @@
  *  Вам  необходимо  суметь встроить её в систему.
  */
 
-require 'functions.php';
+error_reporting(E_ALL);
 
+mb_internal_encoding('utf8');
 session_start();
 
-define ('MAX_WIDTH', 3000);//max image width
-define ('MAX_HEIGHT', 3000);//max image height
-define ('MAX_FILE_SIZE', 3,145728e+7); // changed to 30 MiB
-
-$gallery_content ='';
+require 'functions.php';
 
 
-if ( !isset($_SESSION['username']) )
-{
-    if ( isset( $_COOKIE['username']))
-    {
+define('MAX_WIDTH', 3000);//max image width
+define('MAX_HEIGHT', 3000);//max image height
+define('MAX_FILE_SIZE', 20*1024*1024); //20 MiB //ALSO in php.ini upload_max_filesize=30M-параметр определяет максимальный размер, превышение которого просто не пропускает файл на сервер через форму, передаются только $_FILES['name'] и $_FILE['error'] === 1  ;  and post_max_size=31M -задаёт максимальный размер данных передаваемых метдом POST, при превышении выводится warning о максимально допустимом значении
+
+$gallery_content = '';
+
+// проверка логина
+if (!isset($_SESSION['username'])) {
+    if (isset($_COOKIE['username'])) {
         $_SESSION['username'] = $_COOKIE['username'];
-    }
-    else
-    {
-        header( "Location: auth.php");
+    } else {
+        header("Location: auth.php");
         die();
     }
 }
 
-
-var_dump( $_FILES );
-if ( isset($_FILES['image_file']) )
+// сообщение если размер файла слишком большой
+if ( isset($_SESSION['max_file_size'] ))
 {
-//    foreach ( $_FILES as $uploaded_files )
-//    {
-//        // пропускаем файлы размер которых нам не понравился
-//        if ( $uploaded_files['size'] > MAX_FILE_SIZE )
-//            continue;
-//
-//        // если похоже на картинку то обрабатываем
-//        if (    $uploaded_files['type'] == 'image/jpeg' OR $uploaded_files['type'] == 'image/pjpeg' OR
-//                $uploaded_files['type'] == 'image/bmp' OR $uploaded_files['type'] == 'image/x-windows-bmp' OR
-//                $uploaded_files['type'] == 'image/gif' OR $uploaded_files['type'] == 'image/png'         )
-//        {
-//            var_dump( $uploaded_files);
-//            $img_title = $uploaded_files['name'];
-//            $uploaded_file_moved = "images/{$_SESSION['username']}/{$uploaded_files['name']}";
-//
-//            if ( move_uploaded_file( $uploaded_files['tmp_name'], $uploaded_file_moved ) )
-//            {   //  вызывать создание превью
-//                smart_resize_image ( $uploaded_file_moved, 150, 150, true, "images/{$_SESSION['username']}/preview/".basename($uploaded_file_moved));
-//                $img_preview = "images/{$_SESSION['username']}/preview/".basename($uploaded_file_moved);
-//                $gallery_content .= "<div class=\"image_preview\" > <a href=\"$uploaded_file_moved\" target=\"_blank\" title=\"$img_title\"> <img src=\"$img_preview\"> $img_title</a> </div>";
-//            }
-//        }
-//        else    // не картинка - убиваем чтоб не раздувал /tmp сервака
-//        {
-//            unlink( $uploaded_files['tmp_name'] );
-//        }
-//    }
+    $gallery_content = 'Слишком большой файл, максимум можно '.(MAX_FILE_SIZE/1024/1024).'MiB<br>';
+    unset($_SESSION['max_file_size']);
+}
+
+// строим список файлов в директории, проверяем/создаём превью, формируем контент
+$uploaded_file_path="images/{$_SESSION['username']}";
+foreach (scandir($uploaded_file_path, SCANDIR_SORT_NONE ) as $dir_files) {
+    if ( is_dir("{$uploaded_file_path}/{$dir_files}")  )
+        continue;
+    else
+    {
+        //if ( is_file( "$uploaded_file_path/preview/{$dir_files}" ))
+            $gallery_content .= "<div class=\"image_preview\" >
+                                    <a href=\"$uploaded_file_path/$dir_files\" target=\"_blank\" title=\"$dir_files\">
+                                        <img src=\"$uploaded_file_path/preview/{$dir_files}\">
+                                    {$dir_files}</a>
+                                    </div>";
+        /*else
+        {
+            smart_resize_image("{$uploaded_file_path}/{$dir_files}", 150, 150, true, "$uploaded_file_path/preview/{$dir_files}");
+            $gallery_content .= "<div class=\"image_preview\" >
+                                    <a href=\"$uploaded_file_path/$dir_files\" target=\"_blank\" title=\"$dir_files\">
+                                        <img src=\"$uploaded_file_path/preview/{$dir_files}\">
+                                    {$dir_files}</a>
+                                 </div>";
+        }*/
+
+    }
+
+
+}
+
+//var_dump($_FILES);echo "<br><hr>";
+if (isset($_FILES['image_file'])) {
+    $uploaded_files= $_FILES['image_file'];
+    var_dump($uploaded_files);echo "<br><hr>";
+
+    // пропускаем файлы размер которых нам не понравился
+    if ($uploaded_files['size'] > MAX_FILE_SIZE)
+    {
+        unlink($uploaded_files['tmp_name']);
+        $_SESSION['max_file_size'] = true;
+//        var_dump($_SESSION['max_file_size']);
+        unset( $_FILES['image_file'] );
+        header("location: index.php");
+        die();
+    }
+    // если похоже на картинку то обрабатываем
+    if ($uploaded_files['type'] == 'image/jpeg' OR $uploaded_files['type'] == 'image/pjpeg' OR
+        $uploaded_files['type'] == 'image/bmp' OR $uploaded_files['type'] == 'image/x-windows-bmp' OR
+        $uploaded_files['type'] == 'image/gif' OR $uploaded_files['type'] == 'image/png'
+    ) {
+
+        $img_title = $uploaded_files['name'];
+        $uploaded_file_path = "images/{$_SESSION['username']}/{$img_title}";
+        // stupid windows create files in cp-1251
+        $uploaded_file_path = iconv('windows-1251', 'utf-8', $uploaded_file_path);
+
+        // такого файла ещё нет
+        if ( !is_file($uploaded_file_path) )
+        {
+            // переместили в папку пользователя
+            if (move_uploaded_file($uploaded_files['tmp_name'], $uploaded_file_path))
+            {   //  вызывать создание превью
+                smart_resize_image($uploaded_file_path, 150, 150, true, "images/{$_SESSION['username']}/preview/" . basename($uploaded_file_path));
+                $img_preview = "images/{$_SESSION['username']}/preview/" . basename($uploaded_file_path);
+                $gallery_content .= "<div class=\"image_preview\" >
+                                    <a href=\"$uploaded_file_path\" target=\"_blank\" title=\"$img_title\">
+                                        <img src=\"$img_preview\">
+                                    $img_title</a>
+                                 </div>";
+            }
+        }
+
+    } else    // не картинка - убиваем чтоб не раздувал /tmp сервака
+    {
+        unlink($uploaded_files['tmp_name']);
+    }
+
+    unset($_FILES['image_file']);
 }
 
 
@@ -88,21 +140,21 @@ if ( isset($_FILES['image_file']) )
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="wrap">
-        <h1>Курс PHP1 ДЗ №6 работа с файлами</h1>
-        <div class="auth"><a href="auth.php&action=logout">Выход</a> </div>
-        <div class="gallery">
+<div class="wrap">
+    <h1>Курс PHP1 ДЗ №6 работа с файлами</h1>
 
-            <?php echo $gallery_content; ?>
-            <div class="clear" ></div>
-        </div>
-
-        <form action="" method="post" enctype="multipart/form-data">
-            <input type="file" multiple name="image_file[]">
-            <button type="submit" name="add_file">Добавить</button>
-        </form>
-
+    <div class="gallery">
+        <?php echo $gallery_content; ?>
+        <div class="clear"></div>
     </div>
-    <div class="footer"></div>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="image_file">
+        <button type="submit" name="add_file">Добавить</button>
+    </form>
+    <div class="auth"><a href="auth.php?action=logout">Выход</a></div>
+
+</div>
+<div class="footer"></div>
 </body>
 </html>
